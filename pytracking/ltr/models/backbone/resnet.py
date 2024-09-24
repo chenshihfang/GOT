44dfb2cc9acbc15bc3c2cf50422146ddca1cb79c
sfchen <csf.cs09g@nctu.edu.tw>
2024-09-25 00:44:28 +0800

    commit

---
+++ pytracking/ltr/models/backbone/resnet.py
@@ -0,0 +1,431 @@
+import math
+import torch.nn as nn
+from collections import OrderedDict
+import torch.utils.model_zoo as model_zoo
+from .base import Backbone
+import torch.nn.functional as F
+
+
+import copy
+import os
+import os.path as osp
+
+# from ..transformer.heads import Transformer, LayerNorm
+# from .vit_encoder import build_vit_encoder
+
+import urllib
+from functools import partial
+import torch
+import itertools
+
+
+print_misaligned = 1
+
+def VIT(SEARCH_SIZE, ENCODER_TYPE, ENCODER_PRETRAIN_TYPE, train_encoder):
+
+    encoder = build_vit_encoder(SEARCH_SIZE, ENCODER_TYPE, ENCODER_PRETRAIN_TYPE, train_encoder)
+
+    return encoder
+
+def dinov2(model_name = "dinov2_vitl14"):
+    # dinov2_reg
+
+    dinov2 = torch.hub.load('facebookresearch/dinov2', model_name)
+
+    return dinov2
+
+
+# 3. Define utility functions
+def load_config_from_url(url: str) -> str:
+    with urllib.request.urlopen(url) as f:
+        return f.read().decode()
+
+# 2. Define utility classes
+class CenterPadding(torch.nn.Module):
+    def __init__(self, multiple):
+        super().__init__()
+        self.multiple = multiple
+
+    def _get_pad(self, size):
+        new_size = math.ceil(size / self.multiple) * self.multiple
+        pad_size = new_size - size
+        pad_size_left = pad_size // 2
+        pad_size_right = pad_size - pad_size_left
+        return pad_size_left, pad_size_right
+
+    # @torch.inference_mode()
+    def forward(self, x):
+        pads = list(itertools.chain.from_iterable(self._get_pad(m) for m in x.shape[:1:-1]))
+        output = F.pad(x, pads)
+        return output
+
+
+def resnet50(output_layers=None, pretrained=False, **kwargs):
+    """Constructs a ResNet-50 model.
+    """
+
+    if output_layers is None:
+        output_layers = ['default']
+    else:
+        for l in output_layers:
+            if l not in ['conv1', 'layer1', 'layer2', 'layer3', 'layer4', 'fc']:
+                raise ValueError('Unknown layer: {}'.format(l))
+
+    model = ResNet(Bottleneck, [3, 4, 6, 3], output_layers, **kwargs)
+    if pretrained:
+        try:
+            model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
+        except: 
+            pass
+
+    # model = torchvision.models.resnet50() # note: this could be any model
+    # model = torch.compile(model) # <- magic happens!
+    
+    return model
+
+def conv3x3(in_planes, out_planes, stride=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, bias=False, dilation=dilation)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, dilation=1, use_bn=True):
+        super(BasicBlock, self).__init__()
+        self.use_bn = use_bn
+        self.conv1 = conv3x3(inplanes, planes, stride, dilation=dilation)
+
+        if use_bn:
+            self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes, dilation=dilation)
+
+        if use_bn:
+            self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+
+        if self.use_bn:
+            out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+
+        if self.use_bn:
+            out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, dilation=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=dilation, bias=False, dilation=dilation)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(Backbone):
+    """ ResNet network module. Allows extracting specific feature blocks."""
+    def __init__(self, block, layers, output_layers, num_classes=1000, inplanes=64, dilation_factor=1, frozen_layers=()):
+        self.inplanes = inplanes
+        super(ResNet, self).__init__(frozen_layers=frozen_layers)
+        self.output_layers = output_layers
+        self.conv1 = nn.Conv2d(3, inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        stride = [1 + (dilation_factor < l) for l in (8, 4, 2)]
+        self.layer1 = self._make_layer(block, inplanes, layers[0], dilation=max(dilation_factor//8, 1))
+        self.layer2 = self._make_layer(block, inplanes*2, layers[1], stride=stride[0], dilation=max(dilation_factor//4, 1))
+        self.layer3 = self._make_layer(block, inplanes*4, layers[2], stride=stride[1], dilation=max(dilation_factor//2, 1))
+        self.layer4 = self._make_layer(block, inplanes*8, layers[3], stride=stride[2], dilation=dilation_factor)
+
+        out_feature_strides = {'conv1': 4, 'layer1': 4, 'layer2': 4*stride[0], 'layer3': 4*stride[0]*stride[1],
+                               'layer4': 4*stride[0]*stride[1]*stride[2]}
+
+        # TODO better way?
+        if isinstance(self.layer1[0], BasicBlock):
+            out_feature_channels = {'conv1': inplanes, 'layer1': inplanes, 'layer2': inplanes*2, 'layer3': inplanes*4,
+                               'layer4': inplanes*8}
+        elif isinstance(self.layer1[0], Bottleneck):
+            base_num_channels = 4 * inplanes
+            out_feature_channels = {'conv1': inplanes, 'layer1': base_num_channels, 'layer2': base_num_channels * 2,
+                                    'layer3': base_num_channels * 4, 'layer4': base_num_channels * 8}
+        else:
+            raise Exception('block not supported')
+
+        self._out_feature_strides = out_feature_strides
+        self._out_feature_channels = out_feature_channels
+
+        # self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(inplanes*8 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def out_feature_strides(self, layer=None):
+        if layer is None:
+            return self._out_feature_strides
+        else:
+            return self._out_feature_strides[layer]
+
+    def out_feature_channels(self, layer=None):
+        if layer is None:
+            return self._out_feature_channels
+        else:
+            return self._out_feature_channels[layer]
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilation=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, dilation=dilation))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def _add_output_and_check(self, name, x, outputs, output_layers):
+        if name in output_layers:
+            outputs[name] = x
+        return len(output_layers) == len(outputs)
+
+    def forward(self, x, output_layers=None):
+        """ Forward pass with input x. The output_layers specify the feature blocks which must be returned """
+        outputs = OrderedDict()
+
+        if output_layers is None:
+            output_layers = self.output_layers
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        if self._add_output_and_check('conv1', x, outputs, output_layers):
+            return outputs
+
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+
+        if self._add_output_and_check('layer1', x, outputs, output_layers):
+            return outputs
+
+        x = self.layer2(x)
+
+        if self._add_output_and_check('layer2', x, outputs, output_layers):
+            return outputs
+
+        x = self.layer3(x)
+
+        if self._add_output_and_check('layer3', x, outputs, output_layers):
+            return outputs
+
+        x = self.layer4(x)
+
+        if self._add_output_and_check('layer4', x, outputs, output_layers):
+            return outputs
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        if self._add_output_and_check('fc', x, outputs, output_layers):
+            return outputs
+
+        if len(output_layers) == 1 and output_layers[0] == 'default':
+            return x
+
+        raise ValueError('output_layer is wrong.')
+
+
+def resnet_baby(output_layers=None, pretrained=False, inplanes=16, **kwargs):
+    """Constructs a ResNet-18 model.
+    """
+
+    if output_layers is None:
+        output_layers = ['default']
+    else:
+        for l in output_layers:
+            if l not in ['conv1', 'layer1', 'layer2', 'layer3', 'layer4', 'fc']:
+                raise ValueError('Unknown layer: {}'.format(l))
+
+    model = ResNet(BasicBlock, [2, 2, 2, 2], output_layers, inplanes=inplanes, **kwargs)
+
+    if pretrained:
+        raise NotImplementedError
+    return model
+
+
+def resnet18(output_layers=None, pretrained=False, **kwargs):
+    """Constructs a ResNet-18 model.
+    """
+
+    if output_layers is None:
+        output_layers = ['default']
+    else:
+        for l in output_layers:
+            if l not in ['conv1', 'layer1', 'layer2', 'layer3', 'layer4', 'fc']:
+                raise ValueError('Unknown layer: {}'.format(l))
+
+    model = ResNet(BasicBlock, [2, 2, 2, 2], output_layers, **kwargs)
+
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
+    return model
+
+
+
+
+def resnet101(output_layers=None, pretrained=False, **kwargs):
+    """Constructs a ResNet-101 model.
+    """
+    if output_layers is None:
+        output_layers = ['default']
+    else:
+        for l in output_layers:
+            if l not in ['conv1', 'layer1', 'layer2', 'layer3', 'layer4', 'fc']:
+                raise ValueError('Unknown layer: {}'.format(l))
+
+
+    model = ResNet(Bottleneck, [3, 4, 23, 3], output_layers, **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet101']))
+    return model
+
+
+
+
+from typing import Tuple, Union
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+try:
+    from timm.models.layers import drop, drop_path, trunc_normal_
+except:
+    pass
+
+
+class AttentionPool2d(nn.Module):
+    def __init__(self, spacial_dim: int, embed_dim: int, num_heads: int, output_dim: int = None):
+        super().__init__()
+        self.positional_embedding = nn.Parameter(torch.randn(spacial_dim ** 2 + 1, embed_dim) / embed_dim ** 0.5)
+        self.k_proj = nn.Linear(embed_dim, embed_dim)
+        self.q_proj = nn.Linear(embed_dim, embed_dim)
+        self.v_proj = nn.Linear(embed_dim, embed_dim)
+        self.c_proj = nn.Linear(embed_dim, output_dim or embed_dim)
+        self.num_heads = num_heads
+        self.embed_dim = embed_dim
+        self.spacial_dim = spacial_dim
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        # print("x.shape NCHW", x.shape) # torch.Size([2, 2048, 16, 16])
+        x = x.reshape(x.shape[0], x.shape[1], x.shape[2] * x.shape[3]).permute(2, 0, 1)  # NCHW -> (HW)NC
+        # print("x.shape (HW)NC", x.shape) # torch.Size([256, 2, 2048])
+        # print("x.mean(dim=0, keepdim=True).shape", x.mean(dim=0, keepdim=True).shape) # torch.Size([1, 2, 2048])
+
+        x = torch.cat([x.mean(dim=0, keepdim=True), x], dim=0)  # (HW+1)NC
+
+        # print("x.shape cat", x.shape) # torch.Size([257, 2, 2048])
+
+        cls_pos = self.positional_embedding[0:1, :]
+        spatial_pos = F.interpolate(self.positional_embedding[1:,].reshape(1, self.spacial_dim, self.spacial_dim, self.embed_dim).permute(0, 3, 1, 2), size=(H, W), mode='bilinear')
+        spatial_pos = spatial_pos.reshape(self.embed_dim, H*W).permute(1, 0)
+        positional_embedding = torch.cat([cls_pos, spatial_pos], dim=0)
+
+        x = x + positional_embedding[:, None, :]
+        x, _ = F.multi_head_attention_forward(
+            query=x, key=x, value=x,
+            embed_dim_to_check=x.shape[-1],
+            num_heads=self.num_heads,
+            q_proj_weight=self.q_proj.weight,
+            k_proj_weight=self.k_proj.weight,
+            v_proj_weight=self.v_proj.weight,
+            in_proj_weight=None,
+            in_proj_bias=torch.cat([self.q_proj.bias, self.k_proj.bias, self.v_proj.bias]),
+            bias_k=None,
+            bias_v=None,
+            add_zero_attn=False,
+            dropout_p=0,
+            out_proj_weight=self.c_proj.weight,
+            out_proj_bias=self.c_proj.bias,
+            use_separate_proj_weight=True,
+            training=self.training,
+            need_weights=False
+        )
+
+        # print("x.shape MHSA", x.shape) # torch.Size([257, 2, 1024])
+
+        x = x.permute(1, 2, 0)
+
+        # print("x.shape MHSA permute", x.shape) # torch.Size([2, 1024, 257])
+
+        global_feat = x[:, :, 0]
+        feature_map = x[:, :, 1:].reshape(B, -1, H, W)
+
+        # print("global_feat.shape", global_feat.shape) # torch.Size([2, 1024])
+        # print("feature_map.shape", feature_map.shape) # torch.Size([2, 1024, 16, 16])
+        # exit()
+
+        return global_feat, feature_map
---
+++ pytracking/pytracking/experiments/myexperiments_pivot.py
@@ -0,0 +1,23 @@
+from pytracking.evaluation import Tracker, get_dataset, trackerlist
+import torch
+import numpy as np
+import random
+import os
+
+# conda activate got_pivot
+
+# CUDA_VISIBLE_DEVICES=0 python pytracking/run_experiment.py myexperiments_pivot pivot --debug 0 --threads 2
+
+def pivot():
+
+    trackers = trackerlist('tomp', 'pivotL27', range(1))
+
+    ####################################
+
+    dataset = get_dataset('nfs')
+
+    print("pivot eva load")
+
+    return trackers, dataset
+
+    ####################
